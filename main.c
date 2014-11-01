098cd1af91600e5d287c3b1f445b666811b5c2de
smehemmed <msxiyev@gmail.com>
2014-11-01 20:22:29 +0200

    added main.c Projenin ana başlıkları eklendi :pencil:

---
+++ main.c
@@ -0,0 +1,15 @@
+/*PAU Özgür Yazılım Topluluğunun git &amp; Github eğitimi Banka Otomasyonu projesi.*/
+#include <stdio.h>
+
+/*Prototipler*/
+void Menu();
+
+/*main fonksiyonu*/
+int main() {
+	/*code*/
+	return 0;
+}
+/*Projenin menusu*/
+void Menu() {
+	/*code*/
+}
