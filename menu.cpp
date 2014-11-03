bbb225042fd777cc7591b26b485f3f84e46133cb
sweatclean96 <cetintertemiz@gmail.com>
2014-11-03 19:20:45 +0200

    menu

---
+++ menu.cpp
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<iostream.h>
+
+void menu();
+
+int main()
+{
+
+	int islem;
+
+	
+	menu();
+	scanf("%d", &islem);
+
+	if 
+	{
+		scanf("%d", islem);
+
+
+}
+
+void menu(){
+
+	printf("Bankamiza hosgeldiniz. Islem secimi yapiniz:\n 1.Para Cekme\n 2. Para Yatirma\n 3. Havale\n 4. Bakiye Sorgulama\n 5. Cikis");
+}
